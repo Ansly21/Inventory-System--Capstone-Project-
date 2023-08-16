ea904619fe94433b4cf86df8af170278771464ed
Ansly21 <anslybrandon.dy.iics@ust.edu.ph>
2023-08-16 14:21:35 +0800

    Fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const form = document.getElementById("form")
 const loginBtn = document.getElementById("loginBtn")
 
-loginBtn.addEventListener("click", () => {
+form.addEventListener("submit", () => {
     alert("Login successful!")  
 })
