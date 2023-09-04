70491ebf4d60be51f2eb1b4abe44e6ea0d73557c
jameson3081 <jamesonlouis.celzo.cics@ust.edu.ph>
2023-09-04 19:53:12 +0800

    installed npm pkgs
    npm i bcryptjs body-parser dotenv ejs express jsonwebtoken mongoose morgan multer mysql2 nodemailer

---
+++ app.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+const express = require('express')
+const app = express();
+const path = require ('path');
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+
+
+
+
+//ESTABLISHING MONGODB NODESERVER CONNECTION(CHECK .env)
+mongoose.connect(process.env.DATABASE_URI, {useNewUrlParser: true, useUnifiedTopology: true} )
+const db = mongoose.connection
+
+db.on('error', (err) => {
+    console.log(err)
+})
+
+db.once('open', () => {
+    console.log('MongoDB Server Database Connection Established...')
+})
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+// END OF ESTABLISHING MONGODB CONNECTION
+
+
+
+
+
+
+// Port to listen(AUTOMATIC) OR localhost:3000/
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () =>{
+    console.log(`Server running at port ${PORT}`)
+})
+
+
+app.use('/', pagesRoute) //USE PAGES ROUTE
