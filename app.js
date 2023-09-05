0ba1dc25cfbeaa53a362d47f507f28f5b9441f02
Ansly21 <anslybrandon.dy.iics@ust.edu.ph>
2023-09-05 12:17:59 +0800

    Added ejs

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 require('dotenv').config();
 const express = require('express')
 const app = express();
-const path = require ('path');
+const path = require('path');
 const mongoose = require('mongoose')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 
-
-
-
-//ESTABLISHING MONGODB NODESERVER CONNECTION(CHECK .env file). Enter "npm start" or "node app" to run app.js
-mongoose.connect(process.env.DATABASE_URI, {useNewUrlParser: true, useUnifiedTopology: true} )
+// ESTABLISHING MONGODB NODESERVER CONNECTION(CHECK .env file). Enter "npm start" or "node app" to run app.js
+mongoose.connect(process.env.DATABASE_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 
 db.on('error', (err) => {
@@ -22,12 +19,25 @@ db.once('open', () => {
 })
 
 app.use(morgan('dev'))
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-// END OF ESTABLISHING MONGODB CONNECTION
+
+// Serve static files from the 'public' folder
+app.use(express.static('public'))
+
+// Set the view engine and views directory
+app.set('view engine', 'ejs'); // or your preferred template engine
+app.set('views', path.join(__dirname, 'views')); // Set the views directory
 
 
 
+app.listen(3000, () => {
+    console.log("listening to port 3000")
+});
 
-
-
+//Note: PUBLIC FOLDER = CSS, JS | VIEWS FOLDER = HTML
+// If nodemon app not working, try node app for temporary fix
+//ROUTES
+app.get('/', (req, res) => {
+    res.render('index', {title: "Inventory Management | Login"}); // Renders the 'index.ejs' file in the 'views' directory
+});
