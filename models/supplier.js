8fa47d4934bc2e452fa87fa800e80e3cf1d5d3e4
mars <marcelusandrei.jarabe.cics@ust.edu.ph>
2023-12-01 17:32:03 +0800

    added supplier model

---
+++ models/supplier.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose') 
+
+
+const SchemaTypes = mongoose.Schema.Types;
+
+
+const Schema = mongoose.Schema;
+
+const supplierSchema = new Schema({
+
+    supplierCode:{
+        type:String,
+        required: true
+
+    },
+
+    supplierName:{
+        type: String,
+        required: true
+    },
+
+    brandName:{
+        type: String,
+        required: true
+
+    },
+
+    supplierContact:{
+        type: String,
+        required: false
+    },
+
+    
+
+},  {timestamps: true});
+
+
+const Supplier = mongoose.model('Supplier', supplierSchema);
+module.exports = Supplier;
